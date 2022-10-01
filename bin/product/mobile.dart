2bb722e13fbfa06a1f343d3c5a40315c2409dbb9
Virtue <60309145+Heavendart888@users.noreply.github.com>
2022-10-01 06:34:14 +0530

    Update mobile.dart

--- bin/product/mobile.dart
+++ bin/product/mobile.dart
@@ -1,4 +1,4 @@
-//written below is not an method its and functio as main is not written.
+//written below is not an method its an function as main is not written.
 void samsung() {
   print("i am samsung mobile");
 }
