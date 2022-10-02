42ad96b76841dbd4800f1202ee022441dcd9fcc9
KLH <mynameisbiswajit8@gmail.com>
2022-10-02 09:48:28 +0530

    commit 4

---
+++ bin/part 15 user input.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+//dart:io is an library
+void main() {
+  print("Enter Your Name:");
+  String? name = stdin.readLineSync();
+  print("Welcome to $name");
+}
---
+++ bin/part 16 Assert Statement.dart
@@ -0,0 +1,6 @@
+//It Is Used for Debugging purpose
+void main() {
+  var temp = 40;
+  assert(temp == 40, "this is an error string");
+  print("No error found");
+}
